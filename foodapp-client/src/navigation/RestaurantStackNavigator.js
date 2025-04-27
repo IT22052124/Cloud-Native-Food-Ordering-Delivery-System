cc637d2889010692d8ae67b0e31172e758ed9ead
IT22052124 <it22052124@my.sliit.lk>
2025-04-27 18:51:47 +0530

    navigation update

--- foodapp-client/src/navigation/RestaurantStackNavigator.js
+++ foodapp-client/src/navigation/RestaurantStackNavigator.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import RestaurantsScreen from '../screens/restaurants/RestaurantsScreen';
-import RestaurantDetailScreen from '../screens/restaurants/RestaurantDetailScreen';
-import DishDetailScreen from '../screens/restaurants/DishDetailScreen';
+import RestaurantsScreen from "../screens/restaurants/RestaurantsScreen";
+import RestaurantDetailScreen from "../screens/restaurants/RestaurantDetailScreen";
+import DishDetailScreen from "../screens/restaurants/DishDetailScreen";
+import RestaurantInfoScreen from "../screens/restaurants/RestaurantInfoScreen";
 
 const Stack = createStackNavigator();
 
@@ -15,10 +16,14 @@ const RestaurantStackNavigator = () => {
       }}
     >
       <Stack.Screen name="RestaurantsList" component={RestaurantsScreen} />
-      <Stack.Screen name="RestaurantDetail" component={RestaurantDetailScreen} />
+      <Stack.Screen
+        name="RestaurantDetail"
+        component={RestaurantDetailScreen}
+      />
       <Stack.Screen name="DishDetail" component={DishDetailScreen} />
+      <Stack.Screen name="RestaurantInfo" component={RestaurantInfoScreen} />
     </Stack.Navigator>
   );
 };
 
-export default RestaurantStackNavigator; 
+export default RestaurantStackNavigator;
