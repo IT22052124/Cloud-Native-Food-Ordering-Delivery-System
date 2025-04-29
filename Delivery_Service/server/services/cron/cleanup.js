77a11bcef2252fb18c0527d0db9f430a7c9e6bd7
Shadhir Fawz <129358224+ShadhirFawz@users.noreply.github.com>
2025-04-29 19:51:17 +0530

    Update Delivery_Service directory

--- Delivery_Service/server/services/cron/cleanup.js
+++
@@ -1,7 +0,0 @@
-const { cleanupOldLocations } = require('../redis/driverLocationService');
-
-setInterval(() => {
-  cleanupOldLocations().then(() => {
-    console.log('🧹 Cleaned up stale driver locations');
-  });
-}, 5 * 60 * 1000); // Every 5 minutes
